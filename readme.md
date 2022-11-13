0a7bdf098c75b523a037ea91d013af8d870fd088
Milan Hauth <milahu@gmail.com>
2022-11-13 18:25:21 +0100

    init

---
+++ readme.md
@@ -0,0 +1,63 @@
+# reverse template engine
+
+find a template of many similar html files
+
+## similar projects
+
+- https://github.com/sng2c/tmplrev
+
+### commercial
+
+most commercial scraping services offer this feature
+
+## status
+
+- [x] working prototype
+- [ ] it can find arrays of similar items
+- [ ] it can find trees of similar items
+- [ ] the resulting parser is aware of html syntax
+
+## based on
+
+### tree diff
+
+- https://github.com/search?l=JavaScript&p=2&q=tree+diff&type=Repositories
+- https://github.com/Matt-Esch/virtual-dom
+- https://github.com/syntax-tree/unist-diff - part of unifiedjs
+- https://github.com/Tchanders/treeDiffer.js
+
+### template-based parsing
+
+different from "template finding"
+
+aka: parser-generators
+
+#### javascript
+
+- https://github.com/blerik/razor.js
+- https://github.com/mbrevoort/reverse-string-template
+- https://github.com/akushnikov/template-parser
+- https://github.com/lezer-parser
+
+#### python
+
+- https://github.com/arshaw/scrapemark
+
+## see also
+
+- https://reverseengineering.stackexchange.com/questions/1331/automated-tools-for-file-format-reverse-engineering
+- https://stackoverflow.com/questions/42727092/find-similar-branches-in-multiple-trees
+
+## keywords
+
+- tree compression of many similar trees
+- reverse-engineer the template of many similar html files
+- find template from sample data
+- template-finding
+- template-detection
+- template-generation
+- template-generator
+- grammar generator
+- generate schema from data
+- approximate schema from data
+- find common structure of many similar html files
---
+++ reverse-template.js
@@ -0,0 +1,261 @@
+import {parse} from 'parse5'
+//import {read} from 'to-vfile'
+//import {inspect} from 'unist-util-inspect'
+import {fromParse5} from 'hast-util-from-parse5'
+//import glob from 'tiny-glob'
+import {visit} from 'unist-util-visit'
+//import {toString} from 'hast-util-to-string' // only text contents, no html string
+import {toHtml} from 'hast-util-to-html'
+
+import fs from 'fs'
+
+import reverseStringTemplate from './reverse-string-template/index.js'
+//TypeError: Cannot read properties of undefined (reading 'baz')
+//import * as razor from './razor.js/razor.js'
+
+import Underscore from 'underscore'
+/*
+import handlebars from 'handlebars'
+import Mustache from 'mustache'
+import Dot from 'dot'
+*/
+
+
+
+// TODO
+//const pathGlob = "/home/user/src/milahu/soylent/www.completefoods.co/diy/recipes/*.html"
+
+const d = "/home/user/src/milahu/soylent/www.completefoods.co/diy/recipes"
+
+const leftFile = `${d}/path/to/original-left.html`
+const rightFile = `${d}/path/to/original-right.html`
+
+
+
+Underscore.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+/*
+Underscore.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+const render = Underscore.template("Hello {{ name[0] }}!");
+const result = render({name: ["Mustache"]});
+console.dir({result})
+
+throw new Error("todo")
+
+console.dir({
+  res: Dot.template("{{a}}")({a:1}),
+})
+throw new Error("todo")
+*/
+
+/*
+template engines
+
+Underscore.template works as expected
+{{ name[0] }} is supported
+
+squirrelly scopes data in varName = "it" -> {{it.name1}} instead of {{name1}}
+
+handlebars does not support array data: {{name[0]}} throws parse error
+but nested objects work
+https://handlebarsjs.com/guide/#nested-input-objects
+{{name[0]}} throws: Expecting 'ID', 'STRING', 'NUMBER', 'BOOLEAN', 'UNDEFINED', 'NULL', 'DATA', got 'INVALID'
+{{name.0}} throws: Expecting 'ID', got 'NUMBER'
+supported syntax:
+{{name.[0]}}
+-> ugly
+https://github.com/handlebars-lang/handlebars.js/issues/1322
+
+mustache silently ignores {{name[0]}}
+
+//const render = handlebars.compile(template)
+//const render = (data) => Mustache.render(template, data)
+*/
+
+//console.log(inspect(hast))
+
+//import {h} from 'hastscript'
+import diff from 'unist-diff'
+
+function getAst(source) {
+  const p5ast = parse(source, {
+    sourceCodeLocationInfo: true,
+  })
+  const hast = fromParse5(p5ast)
+  return hast
+}
+
+/*
+async function astOfPath(path) {
+  const file = await read(path)
+  const p5ast = parse(String(file), {
+    sourceCodeLocationInfo: true,
+  })
+  const hast = fromParse5(p5ast, file)
+  return hast
+}
+*/
+
+async function main() {
+
+  let leftSource, rightSource, expectedTemplate
+
+  var isTest = false
+  var isTest = true
+
+  if (isTest) {
+    // generate test sources
+    expectedTemplate = `
+      <html>
+        <head>
+          <title>{{data[0]}}</title>
+        </head>
+        <body>
+          <section>
+            <h1>{{data[1]}}</h1>
+            <div>{{data[2]}}</div>
+          </section>
+        </body>
+      </html>
+    `
+    const dataLength = 3
+    function data(side, length) {
+      length = dataLength
+      return {
+        // left data 0, left data 1, ...
+        data: Array.from({length}).map((_, idx) => `${side} data ${idx}`)
+      }
+    }
+    const render = Underscore.template(expectedTemplate);
+    leftSource = render(data("left"))
+    rightSource = render(data("right"))
+  }
+
+  else {
+    // use real source files
+    leftSource = fs.readFileSync(leftFile, "utf8")
+    rightSource = fs.readFileSync(rightFile, "utf8")
+  }
+
+  const left = getAst(leftSource)
+  const right = getAst(rightSource)
+
+  //console.dir({leftSource, rightSource}, {depth: 3})
+
+  const changes = diff(left, right)
+
+  //delete changes.left; console.dir(changes, {depth: 3})
+
+  //console.dir(Object.keys(changes), {depth: 3})
+
+  /*
+  delete changes.left
+  for (const nodeIdStr of Object.keys(changes)) {
+    const change = changes[nodeIdStr]
+    const nodeIndex = +nodeIdStr
+    console.dir({ nodeIdStr, nodeIndex, change, })
+  }
+  */
+
+  //console.log("left"); console.dir(left, {depth: 8})
+
+  //console.log("replace")
+  // replace
+  let nodeIndex = 0
+  let dataIndex = -1
+  const leftData = []
+  function visitor(node, _index, _parent) {
+    // note: nodeIndex != index
+    nodeIndex++
+    //console.dir({nodeIndex, node}, {depth: 5})
+    if (nodeIndex in changes) {
+      const change = changes[nodeIndex]
+      if (change.type == "text") {
+        //console.log("replace node.children")
+        //console.dir({nodeIndex, node}, {depth: 5})
+        dataIndex++
+        leftData.push(toHtml({type: "root", children: node.children}))
+        node.children = [
+          {
+            type: "text",
+            value: `{{data[${dataIndex}]}}` // {{data[0]}}
+            //value: `{{data_${dataIndex}}}` // {{data_0}}
+          }
+        ]
+      }
+    }
+  }
+  visit(left, visitor)
+
+  function normalizeHtml(source) {
+    return toHtml(getAst(source))
+  }
+
+  const template = toHtml(left)
+
+  if (isTest) {
+    console.log(`expected template:\n${normalizeHtml(expectedTemplate)}\n`)
+  }
+
+  console.log(`template:\n${template}\n`)
+  console.dir({leftData})
+  console.dir({leftData_length: leftData.length})
+
+
+
+  // parse rightData
+
+  // https://github.com/mbrevoort/reverse-string-template
+  const rightData = reverseStringTemplate(normalizeHtml(rightSource), template)
+
+  // https://github.com/blerik/razor.js
+  // TypeError: Cannot read properties of undefined (reading 'data_0')
+  //const rightData = razor.getModel(template, normalizeHtml(rightSource));
+
+
+
+  console.dir({rightData})
+
+  if (isTest == false) {
+    console.log(`writing template.html`)
+    fs.writeFileSync("template.html", template, "utf8")
+
+    console.log(`writing left.html`)
+    fs.writeFileSync("left.html", normalizeHtml(leftSource), "utf8")
+
+    console.log(`writing right.html`)
+    fs.writeFileSync("right.html", normalizeHtml(rightSource), "utf8")
+
+    console.log(`compare:`)
+    console.log(`git diff --color-words=. template.html left.html`)
+    console.log(`git diff --color-words=. template.html right.html`)
+  }
+}
+
+main()
+
+
+
+/*
+var left = h('div', [
+  h('p', ['Some ', h('b', 'importance'), ' and ', h('i', 'emphasis'), '.']),
+  h('pre', h('code', 'foo()'))
+])
+
+var right = h('div', [
+  h('p', [
+    'Some ',
+    h('strong', 'importance'),
+    ' and ',
+    h('em', 'emphasis'),
+    '.'
+  ]),
+  h('pre', h('code', 'bar()'))
+])
+
+console.dir(diff(left, right), {depth: null})
+*/
